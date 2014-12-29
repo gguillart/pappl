070c24e331e5cf13011a4ca84ec1a7bf0d7b8ca0
Yohann <Yohann@pc-yohann.home>
2014-12-29 23:52:52 +0100

    correction faute de frappe

--- src/java/com/ocpappl/bdonn/BDonn.java
+++ src/java/com/ocpappl/bdonn/BDonn.java
@@ -77,6 +77,7 @@ public class BDonn {
                 break;
                 
             case "Personne" : attributs = "(Nom, Prenom, Login, Password)";
+                break;
                 
             case "Enseignant" : attributs = "(Personne_id)";
                 break;
