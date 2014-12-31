9827e3f3524a129be3a39b9c09d1b410ec114a2c
Yohann <Yohann@192.168.1.10>
2014-12-31 16:50:07 +0100

    ajout de la fonction selectionnner

--- src/java/com/ocpappl/bdonn/BDonn.java
+++ src/java/com/ocpappl/bdonn/BDonn.java
@@ -3,132 +3,200 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Yohann
  */
-
 package com.ocpappl.bdonn;
 
 import com.ocpappl.servlets.Creation;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class BDonn {
-    
-    private String login="postgres";
-    private String mdp="EDT";
+
+    private String login = "postgres";
+    private String mdp = "EDT";
     private String dbURL = "jdbc:postgresql://localhost:5432/postgres";
 
-    
-    public Connection connection() throws SQLException{
-        
-        try{
-                Class.forName("org.postgresql.Driver");
-                
-            }
-            catch(java.lang.ClassNotFoundException e){
-                System.err.print("ClassNotFoundException:");
-                System.err.println(e.getMessage());
-            }
-      
-        Connection con = DriverManager.getConnection(dbURL,login,mdp);
+    public Connection connection() throws SQLException {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+        } catch (java.lang.ClassNotFoundException e) {
+            System.err.print("ClassNotFoundException:");
+            System.err.println(e.getMessage());
+        }
+
+        Connection con = DriverManager.getConnection(dbURL, login, mdp);
         return con;
-        
+
     }
-    
-    public void deconnection(Connection con) throws SQLException{
+
+    public void deconnection(Connection con) throws SQLException {
         con.close();
-        
+
         Driver theDriver;
-       try {
-           theDriver = DriverManager.getDriver(dbURL);
-           DriverManager.deregisterDriver(theDriver);
-       } catch (SQLException ex) {
-           Logger.getLogger(Creation.class.getName()).log(Level.SEVERE, null, ex);
-       }
+        try {
+            theDriver = DriverManager.getDriver(dbURL);
+            DriverManager.deregisterDriver(theDriver);
+        } catch (SQLException ex) {
+            Logger.getLogger(Creation.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    
-    
-    public void creer(String type, String valeurs) throws SQLException{
+
+    public void creer(String type, String valeurs) throws SQLException {
         Connection con = connection();
         String attributs = new String();
-        
-        switch (type)
-        {
-            case "Cours" : attributs = "(Type_De_Cours_Nom, Matiere_id, "
-                    + "Enseignant_id, Cours_Date_Debut, Cours_Date_Fin, Salle, "
-                    + "Intervenant, Commentaire)";
+
+        switch (type) {
+            case "Cours":
+                attributs = "(Type_De_Cours_Nom, Matiere_id, "
+                        + "Enseignant_id, Cours_Date_Debut, Cours_Date_Fin, Salle, "
+                        + "Intervenant, Commentaire)";
                 break;
-            
-            case "Matiere" : attributs = "(Matiere_Acronyme, Matiere_Nom)";
+
+            case "Matiere":
+                attributs = "(Matiere_Acronyme, Matiere_Nom)";
                 break;
-                
-            case "Option" : attributs = "(Option_Acronyme, Option_Nom, Responsable_id)";
+
+            case "Option":
+                attributs = "(Option_Acronyme, Option_Nom, Responsable_id)";
                 break;
-                
-            case "Personne" : attributs = "(Nom, Prenom, Login, Password)";
+
+            case "Personne":
+                attributs = "(Nom, Prenom, Login, Password)";
                 break;
-                
-            case "Enseignant" : attributs = "(Personne_id)";
+
+            case "Enseignant":
+                attributs = "(Personne_id)";
                 break;
-                
-            case "ResponsableOption" :  attributs = "(Personne_id)";
+
+            case "ResponsableOption":
+                attributs = "(Personne_id)";
                 break;
-                
-            case "Administrateur" :  attributs = "(Personne_id)";
+
+            case "Administrateur":
+                attributs = "(Personne_id)";
                 break;
-                
-            default :
-                break;  
+
+            default:
+                break;
         }
-           
 
-        String query = "INSERT INTO " + type + attributs + " VALUES(" + valeurs +")";
-          
-        Statement stmt = con.createStatement();
-        stmt.executeUpdate(query);
-        stmt.close();
-        
-        deconnection(con);
-        
-    }
-    
-    public void modifier(String type, String attributsValeurs, String id) throws SQLException{
-        Connection con = connection();
-     
+        String query = "INSERT INTO " + type + attributs + " VALUES(" + valeurs + ");";
 
-        String query = "UPDATE " + type + " SET " + attributsValeurs + " WHERE " 
-                + id;
-          
         Statement stmt = con.createStatement();
         stmt.executeUpdate(query);
         stmt.close();
-        
+
         deconnection(con);
-        
-        
+
     }
-    
-    public void supprimer(String type, String id) throws SQLException{
+
+    public void modifier(String type, String attributsValeurs, String id) throws SQLException {
         Connection con = connection();
-     
-        String query = "DELETE FROM " + type + " WHERE " + id;
-          
+
+        String query = "UPDATE " + type + " SET " + attributsValeurs + " WHERE "
+                + id + ";";
+
         Statement stmt = con.createStatement();
         stmt.executeUpdate(query);
         stmt.close();
-        
+
         deconnection(con);
-        
+
+    }
+
+    public void supprimer(String type, String id) throws SQLException {
+        Connection con = connection();
+
+        String query = "DELETE FROM " + type + " WHERE " + id + ";";
+
+        Statement stmt = con.createStatement();
+        stmt.executeUpdate(query);
+        stmt.close();
+
+        deconnection(con);
+
+    }
+
+    public ArrayList<LinkedList> selectionner(String type) throws SQLException {
+        Connection con = connection();
+        String colonne = new String();
+        String colonne2 = new String();
+        String plus = "";
+        ArrayList<LinkedList> liste = new ArrayList();
+
+        switch (type) {
+            case "Cours": //TODO ?
+                break;
+
+            case "Matiere":
+                colonne = "Matiere_Acronyme";
+                break;
+
+            case "Option":
+                colonne = "Option_Acronyme";
+                break;
+
+            case "Personne":
+                colonne = "Nom";
+                colonne2 = "Prenom";
+                break;
+
+            case "Enseignant":
+                colonne = "Nom";
+                colonne2 = "Prenom";
+                plus = " NATURAL JOIN Personne";
+                break;
+
+            case "ResponsableOption":
+                colonne = "Nom";
+                colonne2 = "Prenom";
+                plus = " NATURAL JOIN Personne";
+                break;
+
+            case "Administrateur":
+                colonne = "nom";
+                colonne2 = "Prenom";
+                plus = " NATURAL JOIN Personne";
+                break;
+
+            default:
+                break;
+        }
+
+        String query = "SELECT * FROM " + type + plus + ";";
+
+        Statement stmt = con.createStatement();
+
+        ResultSet rs = stmt.executeQuery(query);
+
+        for (int i = 0; i < rs.getRow(); i++) {
+            rs.next();
+            LinkedList sousListe = new LinkedList();
+            sousListe.add(rs.getInt(type + "_id"));
+            sousListe.add(rs.getString(colonne));
+            if (colonne2 == "Prenom") {
+                sousListe.add(rs.getString(colonne2));
+            }
+            liste.add(sousListe);
+        }
+
+        stmt.close();
+        deconnection(con);
+
+        return liste;
+
     }
 }
