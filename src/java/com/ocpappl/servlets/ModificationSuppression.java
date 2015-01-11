3d006b49db2a7a63a632d3d93c4b5c5682a04e4c
Yohann <Yohann@pc-yohann.netgear.com>
2015-01-11 18:47:41 +0100

    redirection des liens de modifications(avec choix de l'objet à modifier)

--- src/java/com/ocpappl/servlets/ModificationSuppression.java
+++ src/java/com/ocpappl/servlets/ModificationSuppression.java
@@ -5,8 +5,14 @@
  */
 package com.ocpappl.servlets;
 
+import com.ocpappl.bdonn.BDonn;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,12 +27,60 @@ public class ModificationSuppression extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher("/WEB-INF/modificationsuppression.jsp").forward(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        BDonn edt = new BDonn();
+        ArrayList<LinkedList> liste = new ArrayList();
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Que Modifier ?</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Que voulez-vous modifier ?</h1>");
+
+            switch (request.getParameter("type")) {
+                case "Option":
+
+                    liste = edt.selectionner(request.getParameter("type"));
+                    for (int i = 0; i < liste.size(); i++) {
+                        out.println(liste.get(i).get(1) + " : ");
+                        out.println("<a href=\"/Pappl/modificationOption.jsp?objet=modifier&id=" + liste.get(i).get(0) + "\"> Modifier </a>");
+                        out.println("<a href=\"/Pappl/modificationOption.jsp?objet=supprimer&id=" + liste.get(i).get(0) + "\"> Supprimer </a><br/><br/>");
+                    }
+
+                    break;
+                case "Matiere":
+                    liste = edt.selectionner(request.getParameter("type"));
+                    for (int i = 0; i < liste.size(); i++) {
+                        out.println(liste.get(i).get(1) + " : ");
+                        out.println("<a href=\"/Pappl/modificationMatiere.jsp?objet=modifier&id=" + liste.get(i).get(0) + "\"> Modifier </a>");
+                        out.println("<a href=\"/Pappl/modificationMatiere.jsp?objet=supprimer&id=" + liste.get(i).get(0) + "\"> Supprimer </a><br/><br/>");
+                    }
+                    break;
+                case "Personne":
+                    liste = edt.selectionner(request.getParameter("type"));
+                    for (int i = 0; i < liste.size(); i++) {
+                        out.println(liste.get(i).get(2) + " " + liste.get(i).get(1) + " : ");
+                        out.println("<a href=\"/Pappl/modificationPersonne.jsp?objet=modifier&id=" + liste.get(i).get(0) + "\"> Modifier </a>");
+                        out.println("<a href=\"/Pappl/modificationPersonne.jsp?objet=supprimer&id=" + liste.get(i).get(0) + "\"> Supprimer </a><br/><br/>");
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+            out.println("</body>");
+            out.println("</html>");
+        } catch (SQLException ex) {
+            Logger.getLogger(ModificationSuppression.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-// processRequest(request, response);
+
     }
 }
